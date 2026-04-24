19002e798be3f619a645cf8467d399bd14beb4a1
Veysel Ozturk <oztveysel55@gmail.com>
2026-04-23 22:06:07 -0500

    Add order ID system for better order tracking

--- src/Order.java
+++ src/Order.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
 public class Order {
+    private static int nextId = 1;   // static for unique IDs
+    private int orderId;
     private ArrayList<MenuItem> items;
 
     public Order() {
+        this.orderId = nextId++;
         items = new ArrayList<>();
     }
 
@@ -20,10 +23,13 @@ public class Order {
     }
 
     public void printOrder() {
+        System.out.println("Order ID: " + orderId);
         System.out.println("Order Summary:");
+
         for (MenuItem item : items) {
             System.out.println(item.getName() + " - $" + item.getPrice());
         }
+
         System.out.println("Total: $" + getTotal());
     }
 }
